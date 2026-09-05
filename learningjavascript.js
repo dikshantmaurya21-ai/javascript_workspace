5f5d0f107d7072a4c6ff7bcf57bbf0e9b6130747
dikshantmaurya21-ai <dikshantmaurya21@gmail.com>
2026-09-05 19:10:06 +0530

    learning let, var, const

--- learningjavascript.js
+++ learningjavascript.js
@@ -1,1 +1,19 @@
 console.log("hello")
+const account_id = 3435435
+let accountemail = "dikshant@gmail.com"
+var accountpassword = "2345"
+let accountCity = "delhi"
+// let account_id = 3
+console.log(account_id)
+
+accountemail = "dikshant@123.com"
+accountpassword = "2313"
+accountCity = "uttar pradesh" 
+// accountstate;
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope 
+*/
+
+console.table([accountemail, accountpassword, accountCity])

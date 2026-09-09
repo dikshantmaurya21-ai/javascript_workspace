c433849175c377008ebf8488b64c3e2ed9af0ecb
dikshantmaurya21-ai <dikshantmaurya21@gmail.com>
2026-09-09 18:44:44 +0530

    conversion of any number

---
+++ javascript3.js
@@ -0,0 +1,34 @@
+//let score = "44abs"
+//let score = null
+//let score = undefined
+let score = true
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);// NaN => not a number
+
+// "33" => 44
+// "33abc" => NaN
+// true => 1; false => 0
+
+//let isloggedin = 1      // true
+//let booleanisloggedin = Boolean(isloggedin)
+//console.log(booleanisloggedin)
+
+// let isloggedin =""   // false
+// let booleanisloggedin = Boolean(isloggedin)
+// console.log(booleanisloggedin)
+
+
+    // "dikshant" => true
+let isloggedin ="dikshant"
+let booleanisloggedin = Boolean(isloggedin)
+console.log(booleanisloggedin)
+
+let someNumber = 44
+let stringNumber = String(someNumber)
+console.log( stringNumber)
+console.log(typeof stringNumber)
+

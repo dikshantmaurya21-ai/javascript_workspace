c745db1b86a93a2c06e87f69ea5698cb54fd3652
dikshantmaurya21-ai <dikshantmaurya21@gmail.com>
2026-09-09 17:51:57 +0530

    data types of js

---
+++ javascript2.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version
+//alert(4 + 5); //we are using node.js , not browser
+
+console.log(3 + 5); // code readability is very high
+
+
+let name = "dikshant"
+let age = 19
+let isloggedin = true
+
+/*
+number => 2 to power of 53
+bigint
+string 
+boolean 
+null
+undefined
+symbol => unique
+*/
+// object => key value pair
+
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
